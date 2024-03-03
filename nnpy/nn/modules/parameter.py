cc8a7a1f71ac25647a7a86e5c1612baca064547a
Vadimbuildercxx <38184845+Vadimbuildercxx@users.noreply.github.com>
2024-03-03 22:49:16 +0400

    Fix parameter requires_grad attribute

--- nnpy/nn/modules/parameter.py
+++ nnpy/nn/modules/parameter.py
@@ -4,7 +4,7 @@ import nnpy
 class Parameter(nnpy.Tensor):
     def __init__(self, data: nnpy.Tensor) -> None:
         assert isinstance(data, nnpy.Tensor)
-        super().__init__(data.data)
+        super().__init__(data.data, requires_grad=data.requires_grad)
 
     def parameters(self, params_in: list = None)  -> nnpy.Tensor:
         if params_in is None: params_in = []
